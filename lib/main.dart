73fb8d6cb0fbbbdb3c1a2807fc9308dc55339407
SharpShooter263 <caglar26362@gmail.com>
2025-10-28 02:01:25 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,31 @@
 import 'package:flutter/material.dart';
 import 'screens/login_screen.dart';
+import 'screens/signup_screen.dart';
 
 void main() {
-  runApp(const MySphereApp());
+  runApp(const MyApp());
 }
 
-class MySphereApp extends StatelessWidget {
-  const MySphereApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       title: 'MySphere',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF2F5D89)),
+        inputDecorationTheme: const InputDecorationTheme(
+          border: OutlineInputBorder(),
+        ),
       ),
       home: const LoginScreen(),
+      routes: {
+        '/signup': (context) => const SignUpScreen(),
+        // '/home': (context) => const HomeScreen(), // (ileride ekleriz)
+      },
     );
-  }
+    }
 }
