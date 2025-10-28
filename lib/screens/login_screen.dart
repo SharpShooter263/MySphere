39dd562ffe04211c18ee45f9827f597e20ad8e53
SharpShooter263 <caglar26362@gmail.com>
2025-10-28 03:31:21 +0300

    Update login_screen.dart

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -74,7 +74,7 @@ class _LoginScreenState extends State<LoginScreen> {
               onPressed: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => const SignupScreen()),
+                  MaterialPageRoute(builder: (context) => SignupScreen()),
                 );
               },
               child: const Text('Hesabın yok mu? Kayıt ol'),
