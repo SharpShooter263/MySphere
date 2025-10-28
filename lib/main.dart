44665ac6ff78c20cc3524d8a20543f9b38e28c91
SharpShooter263 <caglar26362@gmail.com>
2025-10-28 07:57:51 +0300

    Firebase initialize kodu eklendi

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'screens/login_screen.dart';
 
-void main() {
-  runApp(const MySphereApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
 }
 
-class MySphereApp extends StatelessWidget {
-  const MySphereApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'MySphere',
       debugShowCheckedModeBanner: false,
+      title: 'MySphere',
       theme: ThemeData(
-        useMaterial3: true,
-        colorSchemeSeed: Colors.indigo,
+        primarySwatch: Colors.blue,
       ),
-      home: const LoginScreen(), // Uygulama Login ekranıyla açılır
+      home: const LoginScreen(),
     );
   }
 }
